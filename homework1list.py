6263001be0e20381848beeebde60e08d3f3d2c5f
olgalupuleac <olga_lupuleac@mail.ru>
2016-09-08 12:35:14 +0300

    First task (lists)

--- homework1list.py
+++ homework1list.py
@@ -10,8 +10,24 @@ def remove_adjacent(lst):
 # Example input: [2, 4, 6], [1, 3, 5]
 # Example output: [1, 2, 3, 4, 5, 6]
 def linear_merge(lst1, lst2):
-    return(lst1+lst2)
-lst1=[1,1,2,4,4]
-lst2=[1,1,2,4,3]
+    i=0
+    j=0
+    lst=[]
+    while (i<len(lst1))and (j<len(lst2)):
+        if (lst1[i]<lst2[j]):
+            lst.append(lst1[i])
+            i=i+1
+        else:
+            lst.append(lst2[j])
+            j=j+1
+    else:
+        if i<len(lst1):
+            lst.extend(lst1[i:])
+        else:
+            lst.extend(lst2[j:])
+            
+    return(lst)
+lst1=[1,1,2,4,4,7]
+lst2=[1,1,2,4,5]
 print(remove_adjacent(lst1))
 print(linear_merge(lst1,lst2))
